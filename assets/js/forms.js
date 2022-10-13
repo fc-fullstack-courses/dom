4b14ce3f104aa18575777c2e3a6bc432e30ef613
Kyrylo <fresh.js.ftd.@gmail.com>
2022-10-13 20:52:24 +0300

    Add unfinisjed todo-list

--- assets/js/forms.js
+++ assets/js/forms.js
@@ -1,31 +1,24 @@
-const form = document.querySelector('#form1');
-// const input = document.querySelector('#input');
+const form = document.querySelector('#form');
 const btn = document.querySelector('#btn');
-const span = document.getElementById('span');
-
-const listener = (e) => {
-  e.stopPropagation(); // отменяет дальнейшее всплытие событий
-  console.log(e.currentTarget);
-};
-
-// btn.addEventListener('click', listener);
-// document.body.addEventListener('click', listener);
-
-form.addEventListener('submit', (e) => {
-  // предотвращает стандартное поведение при исполнении этого события
-  e.preventDefault();
-  const { target: formElem } = e;
-  // console.dir(formElem);
-  // console.log(formElem.elements);
-  // const text = formElem.elements.input.value;
-  console.log(input.value);
-
-  span.textContent = formElem.elements.input.value;
-});
-
+const todo = document.getElementById('todo-list');
 /*
 при отправке формы
 записать в спан значение инпута
 инпут находит через document.* нельзя
 */
-// console.log(input21321.value);
+form.addEventListener('submit', (e) => {
+  // предотвращает стандартное поведение при исполнении этого события
+  e.preventDefault();
+  const { target: formElem } = e;
+
+  // span.textContent = formElem.elements.input.value;
+
+  // создание элемента
+  const testLi = document.createElement('li');
+
+  // заполнить текстом
+  testLi.textContent = formElem.elements.input.value;
+  
+  // вставить лишку в конец формы
+  todo.append(testLi);
+});
