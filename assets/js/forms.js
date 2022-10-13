17f46e135c164aa20d6118c9952d4a1bed5d37a2
Kyrylo <fresh.js.ftd.@gmail.com>
2022-10-13 20:25:42 +0300

    Add form task solution

--- assets/js/forms.js
+++ assets/js/forms.js
@@ -1,17 +1,31 @@
-const form = document.querySelector('#form');
-const input = document.querySelector('#input');
+const form = document.querySelector('#form1');
+// const input = document.querySelector('#input');
 const btn = document.querySelector('#btn');
+const span = document.getElementById('span');
 
 const listener = (e) => {
   e.stopPropagation(); // отменяет дальнейшее всплытие событий
   console.log(e.currentTarget);
 };
 
-btn.addEventListener('click', listener);
-document.body.addEventListener('click', listener);
+// btn.addEventListener('click', listener);
+// document.body.addEventListener('click', listener);
 
 form.addEventListener('submit', (e) => {
   // предотвращает стандартное поведение при исполнении этого события
   e.preventDefault();
+  const { target: formElem } = e;
+  // console.dir(formElem);
+  // console.log(formElem.elements);
+  // const text = formElem.elements.input.value;
   console.log(input.value);
+
+  span.textContent = formElem.elements.input.value;
 });
+
+/*
+при отправке формы
+записать в спан значение инпута
+инпут находит через document.* нельзя
+*/
+// console.log(input21321.value);
